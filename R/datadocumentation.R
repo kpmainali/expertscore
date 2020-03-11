3cc1c06c6a2d4127420570ada79605b30402dbd1
Kumar Mainali <kpmainali@Kumars-iMac.fios-router.home>
2020-03-10 22:02:31 -0400

    Kumar changed name of probability surface, added description of all data

--- R/datadocumentation.R
+++ R/datadocumentation.R
@@ -1,56 +1,126 @@
-#' Occurence data
-#'
-#' A bunch of occurrence data
-#'
-#' @usage
-#' data(occurences)
-#'
-#' @format SpatialPointsDataFrame
-#' \describe{
-#' \item{id}{ID of animal}
-#' \item{start}{Date of beginning of collaring}
-#' \item{end}{Date of death or censoring}
-#' \item{fate}{One of "dead", or "censored"}
-#' }
-#'
-#' @examples
-#' data(occurences)
-#'
-#'
-#' @source This is my source
-#' @keywords data
-"myoccurrences"
-
-
 #' Probability surface
 #'
-#' A bunch of occurrence data
+#' Hypothetical predicted continuous probability surface
 #'
 #' @usage
-#' data(occurences)
+#' data(myprobsurface)
 #'
-#' @format SpatialPointsDataFrame
+#' @format raster
 #' \describe{
-#' \item{id}{ID of animal}
-#' \item{start}{Date of beginning of collaring}
-#' \item{end}{Date of death or censoring}
-#' \item{fate}{One of "dead", or "censored"}
+#'   \item{projection}{"+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=23 +lon_0=-96 +x_0=0 +y_0=0 +ellps=GRS80 +units=m +no_defs"}
 #' }
 #'
 #' @examples
-#' data(occurences)
+#' data(myprobsurface)
 #'
 #'
-#' @source This is my source
+#' @source Hypothetical data
 #' @keywords data
 "myprobsurface"
 
 
-#' Raster memory
-#' 
-#' #' test
-#' 
+#' Expert map #1
+#'
+#' Hypothetical expert map #1
+#'
 #' @usage
-#' data(r_memory)
-#' plot(r_memory)
-"r_memory"
+#' data(myexpert1)
+#'
+#' @format SpatialPolygonsDataFrame
+#' \describe{
+#'   \item{projection}{"+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=23 +lon_0=-96 +x_0=0 +y_0=0 +ellps=GRS80 +units=m +no_defs"}
+#' }
+#'
+#' @examples
+#' data(myexpert1)
+#'
+#'
+#' @source Hypothetical data
+#' @keywords data
+"myexpert1"
+
+
+#' Expert map #2
+#'
+#' Hypothetical expert map #2
+#'
+#' @usage
+#' data(myexpert2)
+#'
+#' @format SpatialPolygonsDataFrame
+#' \describe{
+#'   \item{projection}{"+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=23 +lon_0=-96 +x_0=0 +y_0=0 +ellps=GRS80 +units=m +no_defs"}
+#' }
+#'
+#' @examples
+#' data(myexpert2)
+#'
+#'
+#' @source Hypothetical data
+#' @keywords data
+"myexpert2"
+
+
+#' Expert map #3
+#'
+#' Hypothetical expert map #3
+#'
+#' @usage
+#' data(myexpert3)
+#'
+#' @format SpatialPolygonsDataFrame
+#' \describe{
+#'   \item{projection}{"+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=23 +lon_0=-96 +x_0=0 +y_0=0 +ellps=GRS80 +units=m +no_defs"}
+#' }
+#'
+#' @examples
+#' data(myexpert3)
+#'
+#'
+#' @source Hypothetical data
+#' @keywords data
+"myexpert3"
+
+
+#' Occurence data
+#'
+#' A bunch of hypothetical occurrence records
+#'
+#' @usage
+#' data(occurences)
+#'
+#' @format SpatialPointsDataFrame
+#' \describe{
+#'   \item{projection}{"+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=23 +lon_0=-96 +x_0=0 +y_0=0 +ellps=GRS80 +units=m +no_defs"}
+#' }
+#'
+#' @examples
+#' data(occurences)
+#'
+#' @source Hypothetical data
+#' @keywords data
+"myoccurrences"
+
+
+#' Study area
+#'
+#' Union of all expert maps and convex hull of occurrences (this is one of the methods)
+#'
+#' @usage
+#' data(mystudyarea)
+#'
+#' @format SpatialPolygonsDataFrame
+#' \describe{
+#'   \item{projection}{"+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=23 +lon_0=-96 +x_0=0 +y_0=0 +ellps=GRS80 +units=m +no_defs"}
+#' }
+#'
+#' @examples
+#' data(mystudyarea)
+#'
+#' @source Hypothetical data
+#' @keywords data
+"mystudyarea"
+
+
+
+
